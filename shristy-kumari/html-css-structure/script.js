75523c1f9661cf2ff36d46932368a7d3bcd1f033
Shristy Kumari <shristykumari@Shristys-MacBook-Air.local>
2025-03-19 00:27:38 +0530

    added js file

---
+++ shristy-kumari/html-css-structure/script.js
@@ -0,0 +1,9 @@
+const header=document.getElementById("header");
+const paragraph=document.getElementById("paragraph");
+const btn=document.getElementById("printBtn");
+
+btn.addEventListener("click",()=>{
+    header.style.display="none";
+    paragraph.style.display="none";
+});
+
